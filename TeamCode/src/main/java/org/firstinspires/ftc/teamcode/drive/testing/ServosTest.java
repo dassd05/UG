a073464a87f187a1e58b3fda4954c008c7f39426
Shrest Das <shrest@icloud.com>
2021-06-01 22:39:39 -0500

    bad

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/testing/ServosTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/testing/ServosTest.java
@@ -10,16 +10,18 @@ import com.qualcomm.robotcore.hardware.Servo;
 @TeleOp(group = "testing")
 public class ServosTest extends LinearOpMode {
 
-    Servo shooterStopper, wobbleClawServo1, wobbleClawServo2, shootFlicker, flap, turret, droptakeStopper, wobbleArm;
+    //Servo shooterStopper, wobbleClawServo1, wobbleClawServo2, shootFlicker, flap, turret, droptakeStopper, wobbleArm;
 
-    public static double droptakeStopperVal = -1;
-    public static double wobbleArmVal = -1;
+    Servo wobbleClawServo1, wobbleClawServo2;
+
+//    public static double droptakeStopperVal = -1;
+//    public static double wobbleArmVal = -1;
     public static double wobbleClaw1Val = -1;
     public static double wobbleClaw2Val = -1;
-    public static double shootFlickerVal = -1;
-    public static double shooterStopperVal = -1;
-    public static double flapVal = -1;
-    public static double turretVal = -1;
+//    public static double shootFlickerVal = -1;
+//    public static double shooterStopperVal = -1;
+//    public static double flapVal = -1;
+//    public static double turretVal = -1;
 
     public FtcDashboard dashboard;
 
@@ -27,30 +29,32 @@ public class ServosTest extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
 
-        turret = hardwareMap.get(Servo.class, "turret");
-        flap = hardwareMap.get(Servo.class, "flap");
+//        turret = hardwareMap.get(Servo.class, "turret");
+//        flap = hardwareMap.get(Servo.class, "flap");
         wobbleClawServo1 = hardwareMap.get(Servo.class, "wobbleClawServo1");
         wobbleClawServo2 = hardwareMap.get(Servo.class, "wobbleClawServo2");
-        shootFlicker = hardwareMap.get(Servo.class, "shootFlicker");
-        droptakeStopper = hardwareMap.get(Servo.class, "droptakeStopper");
-        wobbleArm = hardwareMap.get(Servo.class, "wobbleArm");
-        shooterStopper = hardwareMap.get(Servo.class, "shooterStopper");
+//        shootFlicker = hardwareMap.get(Servo.class, "shootFlicker");
+//        droptakeStopper = hardwareMap.get(Servo.class, "droptakeStopper");
+//        wobbleArm = hardwareMap.get(Servo.class, "wobbleArm");
+//        shooterStopper = hardwareMap.get(Servo.class, "shooterStopper");
 
-        Servo[] servos = {shooterStopper, wobbleClawServo1, wobbleClawServo2, shootFlicker, flap, turret, droptakeStopper, wobbleArm};
+        //Servo[] servos = {shooterStopper, wobbleClawServo1, wobbleClawServo2, shootFlicker, flap, turret, droptakeStopper, wobbleArm};
+
+        Servo[] servos = {wobbleClawServo1, wobbleClawServo2};
 
         dashboard = FtcDashboard.getInstance();
 
         waitForStart();
 
         while (opModeIsActive()) {
-            if (flapVal != -1) flap.setPosition(flapVal);
+//            if (flapVal != -1) flap.setPosition(flapVal);
             if (wobbleClaw1Val != -1) wobbleClawServo1.setPosition(wobbleClaw1Val);
             if (wobbleClaw2Val != -1) wobbleClawServo2.setPosition(wobbleClaw2Val);
-            if (shootFlickerVal != -1) shootFlicker.setPosition(shootFlickerVal);
-            if (turretVal != -1) turret.setPosition(turretVal);
-            if (shooterStopperVal != -1) shooterStopper.setPosition(shooterStopperVal);
-            if (droptakeStopperVal != -1) droptakeStopper.setPosition(droptakeStopperVal);
-            if (wobbleArmVal != -1) wobbleArm.setPosition(wobbleArmVal);
+//            if (shootFlickerVal != -1) shootFlicker.setPosition(shootFlickerVal);
+//            if (turretVal != -1) turret.setPosition(turretVal);
+//            if (shooterStopperVal != -1) shooterStopper.setPosition(shooterStopperVal);
+//            if (droptakeStopperVal != -1) droptakeStopper.setPosition(droptakeStopperVal);
+//            if (wobbleArmVal != -1) wobbleArm.setPosition(wobbleArmVal);
 
             telemetry.update();
         }
