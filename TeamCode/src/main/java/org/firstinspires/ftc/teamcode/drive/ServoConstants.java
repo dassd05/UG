2160c1cf6768eb3712633017289bd41c23181e4e
NotAnAlgorithm <ethanfeng42@gmail.com>
2021-06-08 15:16:49 -0500

    shrest bad needs servo values

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/ServoConstants.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.drive;
+
+public class ServoConstants {
+
+    public float dropTakeUp = 0.25f;
+    public float dropTakeDown = 0.0f;
+
+    public float shootBlockerUp = Float.parseFloat(null);
+    public float shootBlockerDown = Float.parseFloat(null);
+
+    public float turretStart = 0.0f;
+    public float turretInsert = 1.0f;
+    public float turretShoot = 0.15f;
+
+    public float flapHG = 0.55f;
+
+    public float wobbleArmUp = 0.2f;
+    public float wobbleArmDown = 0.54f;
+    public float wobbleArmRest = 0.0f;
+
+    public float wobbleClawClose = 0.3f;
+    public float wobbleClawOpen = 0.62f;
+}
