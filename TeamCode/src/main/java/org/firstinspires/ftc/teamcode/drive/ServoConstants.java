71d89c13d0abc63da630ed4110df7044f0398249
NotAnAlgorithm <ethanfeng42@gmail.com>
2021-06-08 16:02:43 -0500

    oop forgot static

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/ServoConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/ServoConstants.java
@@ -2,22 +2,22 @@ package org.firstinspires.ftc.teamcode.drive;
 
 public class ServoConstants {
 
-    public float dropTakeUp = 0.25f;
-    public float dropTakeDown = 0.0f;
+    public static float dropTakeUp = 0.25f;
+    public static float dropTakeDown = 0.0f;
 
-    public float shootBlockerUp = Float.parseFloat(null);
-    public float shootBlockerDown = Float.parseFloat(null);
+    public static float shootBlockerUp = Float.parseFloat(null);
+    public static float shootBlockerDown = Float.parseFloat(null);
 
-    public float turretStart = 0.0f;
-    public float turretInsert = 1.0f;
-    public float turretShoot = 0.15f;
+    public static float turretStart = 0.0f;
+    public static float turretInsert = 1.0f;
+    public static float turretShoot = 0.15f;
 
-    public float flapHG = 0.55f;
+    public static float flapHG = 0.55f;
 
-    public float wobbleArmUp = 0.2f;
-    public float wobbleArmDown = 0.54f;
-    public float wobbleArmRest = 0.0f;
+    public static float wobbleArmUp = 0.2f;
+    public static float wobbleArmDown = 0.54f;
+    public static float wobbleArmRest = 0.0f;
 
-    public float wobbleClawClose = 0.3f;
-    public float wobbleClawOpen = 0.62f;
+    public static float wobbleClawClose = 0.3f;
+    public static float wobbleClawOpen = 0.62f;
 }
